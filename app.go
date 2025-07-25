fbbdf0b866930132dd11814cfa2fa67cbefc0a36
smtdfc <smtdfc.master@gmail.com>
2025-07-25 17:48:39 +0700

    feat: Add App.Start()

--- app.go
+++ app.go
@@ -2,10 +2,17 @@ package photon
 
 type App struct {
 	GlobalData map[string]any
+	Logger *Logger
+}
+
+func (a *App) Start(port string) error{
+	a.Logger.Info("App started successful")
+	return nil
 }
 
 func CreateApp() *App {
 	return &App{
 		GlobalData: make(map[string]any),
+		Logger: CreateLogger("@App"),
 	}
 }
