0ee7b0b16763b08bc0747d4b8283fdd9fead128a
smtdfc <smtdfc.master@gmail.com>
2025-07-25 17:51:01 +0700

    fix: Fix wrong log message

--- logger.go
+++ logger.go
@@ -10,23 +10,23 @@ type Logger struct {
 }
 
 func (l *Logger) Info(msg string) {
-	log.Printf("[INFO ][@modules/%s] %s\n", l.Target, msg)
+	log.Printf("[INFO ][%s] %s\n", l.Target, msg)
 }
 
 func (l *Logger) Warn(msg string) {
-	log.Printf("[WARN ][@modules/%s] %s\n", l.Target, msg)
+	log.Printf("[WARN ][%s] %s\n", l.Target, msg)
 }
 
 func (l *Logger) Error(msg string) {
-	log.Printf("[ERROR][@modules/%s] %s\n", l.Target, msg)
+	log.Printf("[ERROR][%s] %s\n", l.Target, msg)
 }
 
 func (l *Logger) Debug(msg string) {
-	log.Printf("[DEBUG][@modules/%s] %s\n", l.Target, msg)
+	log.Printf("[DEBUG][%s] %s\n", l.Target, msg)
 }
 
 func (l *Logger) Fatal(msg string) {
-	log.Printf("[FATAL][@modules/%s] %s\n", l.Target, msg)
+	log.Printf("[FATAL][%s] %s\n", l.Target, msg)
 	os.Exit(1)
 }
 
